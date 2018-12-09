a6cf87213b9fcbe746912ef3225f3de6a14b4d16
Kaushik <45584338+opensourcey@users.noreply.github.com>
2018-12-09 16:56:08 +0530

    Create main_activity.java

---
+++ main_activity.java
@@ -0,0 +1,8 @@
+@Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent exit = new Intent(Intent.ACTION_MAIN);
+        exit.addCategory(Intent.CATEGORY_HOME);
+        exit.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(exit);
+    }
